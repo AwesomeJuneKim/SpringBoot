debaec3bab14ab1876fb12345311a744be8ddebb
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-05-16 13:10:00 +0900

    jpa 쿼리메서드

--- Chapter05/src/main/java/edu/pnu/persistence/BoardRepository.java
+++ Chapter05/src/main/java/edu/pnu/persistence/BoardRepository.java
@@ -9,7 +9,8 @@ import edu.pnu.domain.Board;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 	//jpa가 인터페이스의 메서드를 자동으로 생성해서 컨테이너에 올려주기 때문에 어노테이션을 입력할 필요가 없다.
-	
+
+	//인터페이스에서 제공하는 메서드외에 다른 메서드를 만들어서 사용하고 싶다면 쿼리메서드를 작성한다.
 	List<Board> findByTitle(String searchKeyword);
 	
 	List<Board> findByContentContaining(String searchKeyword);
